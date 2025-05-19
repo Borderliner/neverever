d7e633873a0af472b022ec8894717362cdb4e1db
Mohammadreza Hajianpour <hajianpour.mr@gmail.com>
2025-05-19 11:00:13 +0330

    fix: coverage settings issue

--- jest.config.ts
+++ jest.config.ts
@@ -24,13 +24,18 @@ const config: Config.InitialOptions = {
   // Enable ES Modules support
   extensionsToTreatAsEsm: ['.ts'],
 
-  // Optional: Ignore node_modules except for specific packages if needed
+  // Ignore node_modules except for specific packages if needed
   transformIgnorePatterns: ['/node_modules/'],
 
-  // Optional: Ensure module resolution aligns with TypeScript
+  // Ensure module resolution aligns with TypeScript
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1', // Handle .js extensions in imports
   },
+
+  // Coverage settings
+  coverageReporters: ['text', 'json-summary', 'lcov'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
 }
 
 export default config
